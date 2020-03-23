e6efe867dec91efeea5740bf27b8a851ebb0f23b
Ben Prothe <beprothe@gmail.com>
2020-03-22 20:12:09 -0400

    added frontend folder to templates. installed vue js

---
+++ templates/frontend/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Beef or chicken say yes"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    
+  },
+  data() {
+    return {
+      // key:value pairs that you can access in components
+    }
+  }
+}
+</script>
