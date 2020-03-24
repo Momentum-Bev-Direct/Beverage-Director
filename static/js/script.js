95f89b269dc98df4881851757ee5d9327976151e
nwlilley <nathaniel.lilley@gmail.com>
2020-03-24 15:00:49 -0400

    slider populating fields

--- static/js/script.js
+++ static/js/script.js
@@ -3,9 +3,20 @@ console.log('js linked wubba')
 const slider = document.querySelector('#ounce-slider')
 
 const ounceOutput = document.querySelector('#ounce-pour')
-const pourPriceOutput = document.querySelector('#price-per-pour')
+const pourPriceOutput = document.querySelector('#pour-price')
 ounceOutput.innerHTML = slider.value
+const costOfGoodsOutput = document.querySelector('#cost-of-goods-value')
+const menuPriceOutput = document.querySelector('#menu-price-value')
+const targetCostPercent = document.querySelector('#target-cost-value')
+let pricePerOunce = .50
+
 
 slider.oninput = function () {
     ounceOutput.innerHTML = this.value;
+    pourPriceOutput.innerHTML = (this.value * pricePerOunce).toFixed(2)
+    costOfGoodsOutput.innerHTML = pourPriceOutput.innerHTML
+    menuPriceOutput.innerHTML = (costOfGoodsOutput.innerHTML / (targetCostPercent.value / 100)).toFixed(2)
+
+    
 }
+
