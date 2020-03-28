8a2adc3ff869701a6efbce5e310e760b0cd9f0da
Ben Prothe <beprothe@gmail.com>
2020-03-28 18:19:41 -0400

    created dict of category types for spirits. also added mystock to models list

--- bevdir/models.py
+++ bevdir/models.py
@@ -1,9 +1,130 @@
 from django.db import models
 from users.models import User
 
+CATEGORIES = [
+    {
+        "cat": "Bourbon",
+        "min": 0,
+        "max": 99,
+    },{
+        "cat": "Whiskey",
+        "min": 100,
+        "max": 199,
+    },{
+        "cat": "Scotch",
+        "min": 200,
+        "max": 299,
+    },{
+        "cat": "Gin",
+        "min": 300,
+        "max": 399,
+    },{
+        "cat": "Vodka",
+        "min": 400,
+        "max": 499,
+    },{
+        "cat": "Rum",
+        "min": 600,
+        "max": 649,
+    },{
+        "cat": "Brandy & Cognac",
+        "min": 650,
+        "max": 699,
+    },{
+        "cat": "Cordials & Liqueurs",
+        "min": 700,
+        "max": 799,
+    },{
+        "cat": "Tequila & Mezcal",
+        "min": 800,
+        "max": 899,
+    },{
+        "cat": "Other",
+        "min": 19700,
+        "max": 21099,
+    },{
+        "cat": "Mini",
+        "min": 21100,
+        "max": 22050,
+    },{
+        "cat": "Bourbon",
+        "min": 22050,
+        "max": 22299,
+    },{
+        "cat": "Rye",
+        "min": 22300,
+        "max": 22899,
+    },{
+        "cat": "Bourbon",
+        "min": 22900,
+        "max": 27999,
+    },{
+        "cat": "Whiskey - Other",
+        "min": 29400,
+        "max": 33499,
+    },{
+        "cat": "Scotch",
+        "min": 33600,
+        "max": 36999,
+    },{
+        "cat": "Canadian Whiskey",
+        "min": 37700,
+        "max": 39050,
+    },{
+        "cat": "Irish Whiskey",
+        "min": 40000,
+        "max": 40249,
+    },{
+        "cat": "Whiskey - Other",
+        "min": 40250,
+        "max": 40279,
+    },{
+        "cat": "Gin",
+        "min": 40280,
+        "max": 43199,
+    },{
+        "cat": "Vodka",
+        "min": 43250,
+        "max": 47006,
+    },{
+        "cat": "Rum",
+        "min": 47017,
+        "max": 49499,
+    },{
+        "cat": "Brandy & Cognac",
+        "min": 49500,
+        "max": 51040,
+    },{
+        "cat": "Other",
+        "min": 51400,
+        "max": 65613,
+    },{
+        "cat": "Cordials & Liqueurs",
+        "min": 56700,
+        "max": 61999,
+    },{
+        "cat": "Other",
+        "min": 62044,
+        "max": 62199,
+    },{
+        "cat": "Cordials & Liqueurs",
+        "min": 62200,
+        "max": 64049,
+    },{
+        "cat": "Tequila & Mezcal",
+        "min": 64050,
+        "max": 65999,
+    },{
+        "cat": "NC Products",
+        "min": 66000,
+        "max": 69999,
+    },{
+        "cat": "Vodka",
+        "min": 70000,
+        "max": 70200,
+    },
+]
 
-
-# BRANDNAMES = tuple([(spirit['brandname'], spirit['brandname']) for spirit in SPIRITS_LIST])
 
 class Spirit(models.Model):
     brandname = models.CharField(max_length=100)
@@ -24,6 +145,21 @@ class Spirit(models.Model):
         price_per_oz = round(float(self.mxb)/bottle_size_oz, 2)
         return price_per_oz
 
+    @property
+    def category(self):
+        code = int(self.nc_code.replace('-', ''))
+        for category in CATEGORIES:
+            if code >= category["min"] and code <= category["max"]:
+                return category["cat"]
+        else:
+            return "Category Not Found"
+
+class MyStock(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    spirit = models.ForeignKey(Spirit, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.spirit.brandname}'
 
 
 class MiscIngredient(models.Model):
